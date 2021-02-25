786cd29100e86b23f049a926c6b0a331b98f15d3
life-aquatic <f.a.nikitin@gmail.com>
2021-02-25 23:43:00 +0300

    first

---
+++ boolfuc/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Numerics;
+
+namespace boolfuc
+{
+    public class Tape
+    {
+        public int Cursor = 0;
+        //TODO - remove 999 bit limit
+        public BitArray Positive = new BitArray(999);
+        public BitArray Negative = new BitArray(999);
+        public (BitArray stor, int ind) UnsigifyStorage(int cursor)
+        {
+            return cursor < 0 ? (Negative, cursor * -1 - 1) : (Positive, cursor);
+        }
+        public bool ReadBit(int index)
+        {
+            var storageInd = UnsigifyStorage(index);
+            return storageInd.stor[storageInd.ind];
+        }
+
+
+        public void FlipCursor()
+        {
+            var storageInd = UnsigifyStorage(Cursor);
+            storageInd.stor[storageInd.ind] = !storageInd.stor[storageInd.ind];
+        }
+        public void WriteCursor(bool value)
+        {
+            var storageInd = UnsigifyStorage(Cursor);
+            storageInd.stor[storageInd.ind] = value;
+        }
+    }
+    public class InputBuffer
+    {
+        //TODO: if end of program has been reached, do not ask for input, but pad the rest of this with zeros
+        public BitArray readBuffer = new BitArray(0);
+        int inputCursor = -1;
+
+        public bool OneBitFromBuffer()
+        {
+            while (inputCursor >= readBuffer.Length - 1)
+            {
+                string input = Console.ReadLine();
+                readBuffer = new BitArray(Encoding.ASCII.GetBytes(input.ToCharArray()));
+                inputCursor = -1;
+            }
+            inputCursor += 1;
+            return readBuffer[inputCursor];
+        }
+    }
+    public class OutputBuffer
+    {
+        //TODO: send whatever we have (padded with zeros) to stdout when end of program is reached
+        public BitArray outBuffer = new BitArray(8);
+        int outputCursor = 0;
+        public void Dump(bool value)
+        {
+            if (outputCursor < 8)
+            {
+                outBuffer[outputCursor] = value;
+                outputCursor += 1;
+            }
+            if (outputCursor == 8)
+            {
+                byte[] outputByte = new byte[1];
+                outBuffer.CopyTo(outputByte, 0);
+                Console.Write(Encoding.ASCII.GetString(outputByte));
+                outputCursor = 0;
+                outBuffer[outputCursor] = value;
+            }
+        }
+    }
+
+    public class OutputBufferSimple
+    {
+        //TODO: send whatever we have (padded with zeros) to stdout when end of program is reached
+        public BitArray outBuffer = new BitArray(8);
+        int outputCursor = 0;
+        public void Dump(bool value)
+        {
+            if (outputCursor < 8)
+            {
+                outBuffer[outputCursor] = value;
+                outputCursor += 1;
+            }
+            if (outputCursor == 8)
+            {
+                byte[] outputByte = new byte[1];
+                outBuffer.CopyTo(outputByte, 0);
+                Console.Write(Encoding.ASCII.GetString(outputByte));
+                outputCursor = 0;
+                outBuffer[outputCursor] = value;
+            }
+        }
+    }
+
+
+
+
+    public static class FlowOfControl
+    {
+        public static int JumpCursor(string program, int cursor, bool backwards = false)
+        {
+            (char openingBrac, char closingBrac, int direction) instructions = backwards ? ('[', ']', -1) : (']', '[', 1);
+            int moreBracketsToSkip = 0;
+            bool foundCounterpart = false;
+            while (!foundCounterpart | moreBracketsToSkip > 0)
+            {
+                if (program[cursor] == instructions.openingBrac)
+                {
+                    foundCounterpart = true;
+                    if (moreBracketsToSkip > 0)
+                        moreBracketsToSkip -= 1;
+                }
+                else if (program[cursor] == instructions.closingBrac)
+                {
+                    moreBracketsToSkip += 1;
+                }
+                cursor += instructions.direction;
+            }
+            return cursor;
+        }
+    }
+    public class Boolfuck
+    {
+
+        public static string interpret(string code, string input)
+        {
+            int EOF = code.Length;
+            int cursor = 0;
+            Tape tape = new Tape();
+            Queue<bool> outBufferSimple = new Queue<bool>();
+            //OutputBuffer outputBuffer = new OutputBuffer();
+            InputBuffer inputBuffer = new InputBuffer();
+            while (cursor < EOF)
+            {
+                switch (code[cursor])
+                {
+                    case '+':
+                        tape.FlipCursor();
+                        break;
+                    case ';':
+                        outBufferSimple.Enqueue(tape.ReadBit(tape.Cursor));
+                        break;
+                    case ',':
+                        tape.WriteCursor(inputBuffer.OneBitFromBuffer());
+                        break;
+                    case '>':
+                        tape.Cursor += 1;
+                        break;
+                    case '<':
+                        tape.Cursor -= 1;
+                        break;
+                    case '[':
+                        if (!tape.ReadBit(tape.Cursor))
+                            cursor = FlowOfControl.JumpCursor(code, cursor);
+                        break;
+                    case ']':
+                        cursor = FlowOfControl.JumpCursor(code, cursor, true) + 1;
+                        break;
+                    default:
+                        break;
+                }
+                cursor += 1;
+            }
+            foreach (bool i in Enumerable.Repeat(false, 8 - (outBufferSimple.Count % 8)))
+                outBufferSimple.Enqueue(i);
+            //byte[] outputByte = new byte[outBufferSimple.Count / 8];
+            //outBufferSimple.CopyTo(outputByte, 0);
+            //Console.Write(Encoding.ASCII.GetString(outputByte));
+            return "ff";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string bractest = "01[3456]89[1[[45]]]9012[[56]]9";
+            //string hw = ";;;+;+;;+;+;+;+;+;+;;+;;+;;;+;;+;+;;+;;;+;;+;+;;+;+;;;;+;+;;+;;;+;;+;+;+;;;;;;;+;+;;+;;;+;+;;;+;+;;;;+;+;;+;;+;+;;+;;;+;;;+;;+;+;;+;;;+;+;;+;;+;+;+;;;;+;+;;;+;+;+;";
+            //string prog = ",>,>,>,>,>,>,>,<<<<<<<;>;>;>;>;>;>;>;";
+            string hwbrac = ";;;+;+;;+;+;" +    //H
+            "+; +; +; +; ; +; ; +; " +          //e
+             "; ; +; ; +; +; ; +; " +           //l
+             "; +; +; +;+;;;; " +                //newline
+             "; ; +; ; +; +; ; +; " +           //l
+             "+; ; ; ; +; +; ; +; " +           //o
+             "; +; +; +;+;;;; " +               //newline
+             "; ; +; ; +; +; +; ; " +           //,
+             "; ; ; ; ; +; +; ; " +             //space
+             "+; ; ; +; +; ; ; +; " +           //w
+             "+; ; ; ; +; +; ; +; " +           //o
+             "; +; +; +;+;;;; " +               //space
+             "; +; +; ; +; ; ; +; " +           //r
+             "; ; +; ; +; +; ; +; " +           //l
+             "; ; +; +; ; +; ; +; " +           //d
+             "+; +; ; ; ; +; +; ; " +           //!
+             "; +; +; +; ";
+            //int EOF = hwbrac.Length;
+            //int cursor = 0;
+            //Tape tape = new Tape();
+            //OutputBuffer outputBuffer = new OutputBuffer();
+            //InputBuffer inputBuffer = new InputBuffer();
+            //while (cursor < EOF)
+            //{
+            //    //TODO: replace hwbrac with a variable
+            //    switch (hwbrac[cursor])
+            //    {
+            //        case '+':
+            //            tape.FlipCursor();
+            //            break;
+            //        case ';':
+            //            outputBuffer.Dump(tape.ReadBit(tape.Cursor));
+            //            break;
+            //        case ',':
+            //            tape.WriteCursor(inputBuffer.OneBitFromBuffer());
+            //            break;
+            //        case '>':
+            //            tape.Cursor += 1;
+            //            break;
+            //        case '<':
+            //            tape.Cursor -= 1;
+            //            break;
+            //        case '[':
+            //            if (!tape.ReadBit(tape.Cursor))
+            //                cursor = FlowOfControl.JumpCursor(hwbrac, cursor);
+            //            break;
+            //        case ']':
+            //            cursor = FlowOfControl.JumpCursor(hwbrac, cursor, true) + 1;
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //    cursor += 1;
+            //}
+        }
+    }
+}
---
+++ test-boolfuc/SolutionTest.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace boolfuc
+{
+    [TestClass]
+    public class SolutionTest
+
+    {
+        Tape tape = new Tape();
+
+        [TestMethod]
+        public void TestReadCursor()
+        {
+            tape.Positive = new BitArray(new int[] { 262733568 });
+            tape.Negative = new BitArray(new int[] { 262733568 });
+            //00001111 10101000 11111111 00000000
+
+            tape.Cursor = 0;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+            tape.Cursor = 7;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+            tape.Cursor = 8;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), true);
+            tape.Cursor = 15;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), true);
+            tape.Cursor = 16;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+            tape.Cursor = -1;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+            tape.Cursor = -8;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+            tape.Cursor = -9;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), true);
+            tape.Cursor = -16;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), true);
+            tape.Cursor = -17;
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+        }
+
+        [TestMethod]
+        public void TestFlipCursor()
+        {
+            tape.Positive = new BitArray(new int[] { 262733568 });
+            tape.Negative = new BitArray(new int[] { 262733568 });
+            //00001111 10101000 11111111 00000000
+
+            tape.Cursor = 0;
+            tape.FlipCursor();
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), true);
+            tape.FlipCursor();
+            Assert.AreEqual(tape.ReadBit(tape.Cursor), false);
+        }
+        [TestMethod]
+        public void TestInputBuffer()
+        {
+            InputBuffer inputBuffer = new InputBuffer();
+            inputBuffer.readBuffer = new BitArray(new byte[] { 97, 98, 99 });
+            List<bool> testBits = new List<bool>() { true, false, false, false, false, true, true, false,
+             false, true, false, false, false, true, true, false,
+             true, true, false, false, false, true, true, false };
+            //abc
+            //97,98,99
+            //("True ", "False", "False ", "False ", "False ", "True ", "True ", "False", 
+            // "False", "True ", "False ", "False ", "False ", "True ", "True ", "False", 
+            // "True ", "True ", "False ", "False ", "False ", "True ", "True ", "False")
+            foreach (bool i in testBits)
+            {
+                Assert.AreEqual(i, inputBuffer.OneBitFromBuffer());
+            }
+            var emulatedInput = new System.IO.StringReader("abc");
+            Console.SetIn(emulatedInput);
+            foreach (bool i in testBits)
+            {
+                Assert.AreEqual(i, inputBuffer.OneBitFromBuffer());
+            }
+
+        }
+
+
+
+    }
+    [TestClass]
+    public class OutputTests
+    {
+        public System.IO.StringWriter w;
+        private string _consoleOutput;
+        OutputBuffer outputBuffer;
+
+        List<bool> testBits24 = new List<bool>()
+                { true, false, false, false, false, true, true, false,
+             false, true, false, false, false, true, true, false,
+             true, true, false, false, false, true, true, false };
+
+        List<bool> testBits23 = new List<bool>()
+                { true, false, false, false, false, true, true, false,
+             false, true, false, false, false, true, true, false,
+             true, true, false, false, false, true, false };
+
+        List<bool> testBits0 = new List<bool>()
+        { };
+
+        public string TestOutputBuffer(List<bool> outputB)
+        {
+            outputBuffer = new OutputBuffer();
+            w = new System.IO.StringWriter();
+            Console.SetOut(w);
+            foreach (var i in outputB)
+            {
+                outputBuffer.Dump(i);
+            }
+            _consoleOutput = w.GetStringBuilder().ToString().Trim();
+            return _consoleOutput;
+        }
+        [TestMethod]
+        public void TestOutputs()
+        {
+            Assert.AreEqual(TestOutputBuffer(testBits24), "abc");
+            Assert.AreEqual(TestOutputBuffer(testBits23), "ab");
+            Assert.AreEqual(TestOutputBuffer(testBits0), "");
+        }
+
+
+    }
+    [TestClass]
+    public class TestFlowOfControl
+    {
+        [TestMethod]
+        public void TestJumpCursor()
+        {
+            Assert.AreEqual(FlowOfControl.JumpCursor("abcd[ed]fhg", 4), 8);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 2), 8);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 10), 19);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 13), 17);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 23), 29);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 24), 28);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 28, true), 22);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 7, true), 1);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 18, true), 9);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 17, true), 11);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 16, true), 12);
+            Assert.AreEqual(FlowOfControl.JumpCursor("01[3456]89[1[[45]]]9012[[56]]9", 28, true), 22);
+        }
+    }
+}
+
