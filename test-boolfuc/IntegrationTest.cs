6b03a1562751bda6485bfa9e90a5297546a4b46c
life-aquatic <f.a.nikitin@gmail.com>
2021-03-14 22:06:59 +0300

    added tests for entire program

---
+++ test-boolfuc/IntegrationTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace boolfuc
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestMethod]
+        public void testEmpty()
+        {
+            Assert.AreEqual("", Boolfuck.interpret("", ""));
+            //Assert.AreEqual("", Boolfuck.interpret(Brainfuck.toBoolfuck(""), ""));
+        }
+
+        [TestMethod]
+        public void testSingleCommands()
+        {
+            Assert.AreEqual("", Boolfuck.interpret("<", ""));
+            Assert.AreEqual("", Boolfuck.interpret(">", ""));
+            Assert.AreEqual("", Boolfuck.interpret("+", ""));
+            Assert.AreEqual("", Boolfuck.interpret(".", ""));
+            Assert.AreEqual("\u0000", Boolfuck.interpret(";", ""));
+        }
+        //[TestMethod]
+        //public void testIO()
+        //{
+        //    Assert.AreEqual("*", Boolfuck.interpret(Brainfuck.toBoolfuck(",."), "*"));
+        //}
+        [TestMethod]
+        public void testHelloWorld()
+        {
+            Assert.AreEqual("Hello, world!\n", Boolfuck.interpret(";;;+;+;;+;+;+;+;+;+;;+;;+;;;+;;+;+;;+;;;+;;+;+;;+;+;;;;+;+;;+;;;+;;+;+;+;;;;;;;+;+;;+;;;+;+;;;+;+;;;;+;+;;+;;+;+;;+;;;+;;;+;;+;+;;+;;;+;+;;+;;+;+;+;;;;+;+;;;+;+;+;", ""));
+        }
+        [TestMethod]
+        public void testBasic()
+        {
+            //Assert.AreEqual("Codewars", Boolfuck.interpret(">,>,>,>,>,>,>,>,<<<<<<<[>]+<[+<]>>>>>>>>>[+]+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+]<<<<<<<<;>;>;>;>;>;>;>;<<<<<<<,>,>,>,>,>,>,>,<<<<<<<[>]+<[+<]>>>>>>>>>[+]+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]", "Codewars\u00ff"));
+            //Assert.AreEqual("Codewars", Boolfuck.interpret("+[>];;;+;+;;+;+;", ""));
+            Assert.AreEqual("Codewars", Boolfuck.interpret(">,>,>,>,>,>,>,>,>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>;>;>;>;>;>;>;>;>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+]+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]>,>,>,>,>,>,>,>,>+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]", "Codewars"));
+            //Assert.AreEqual("\u0048", Boolfuck.interpret(">,>,>,>,>,>,>,>,>>,>,>,>,>,>,>,>,<<<<<<<<+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>>>>>>>>>>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+]>[>]+<[+<]>>>>>>>>>[+]>[>]+<[+<]>>>>>>>>>[+]<<<<<<<<<<<<<<<<<<+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]>>>>>>>>>>>>>>>>>>>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+]<<<<<<<<<<<<<<<<<<<<<<<<<<[>]+<[+<]>>>>>>>>>[+]>>>>>>>>>>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]<<<<<<<<<<<<<<<<<<+<<<<<<<<+[>+]<[<]>>>>>>>>>[+]+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]>>>>>>>>>>>>>>>>>>>;>;>;>;>;>;>;>;<<<<<<<<", "\u0008\u0009"));
+        }
+    }
+}
